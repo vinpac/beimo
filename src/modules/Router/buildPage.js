fbd186ae6a508f13983cfa605ab78ae4ec20267a
Vinicius Pacheco <vin175pacheco@gmail.com>
2017-12-12 20:36:57 -0200

    Changes how page id is generated

--- src/modules/Router/buildPage.js
+++ src/modules/Router/buildPage.js
@@ -1,7 +1,7 @@
 const hash = require('string-hash')
 
 module.exports = (chunkName, props) => {
-  props.id = String(hash(chunkName))
+  props.id = String(hash(`${chunkName}${props.path}${props.exact ? '1' : '0'}`))
 
   props.chunkName = chunkName
   props.exact = !!(props.path && props.exact !== false)
